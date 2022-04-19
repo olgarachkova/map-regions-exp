470e11ad5e52c60dfb3d33781f8b74c59879b720
olga rachkova <olgarachkova96@gmail.com>
2022-04-19 17:21:25 +0700

    rivers script

--- src/back/parser/rivers.ts
+++ src/back/parser/rivers.ts
@@ -14,22 +14,34 @@ async function main() {
     let stmt = await statement.getStmt();
     (global as any).stmt = stmt;
 
-    /*let q = "";
-    let rows = await stmt.fetchList(q);*/
-
     for (let i = 0; i < russianRivers.length; i++) {
         let url = `https://nominatim.openstreetmap.org/search?format=json&q=${russianRivers[i]}&polygon_geojson=1`;
         curl.init(url);
 
         let res = await curl.exec();
         try {
-            let json = JSON.parse(res);
-            console.log(json);
+            let jsonRes = JSON.parse(res);
+
+
+
+
+            for (let j = 0; j < jsonRes.length; j++) {
+                if (jsonRes[j].type === 'river') {
+                    let name = JSON.stringify(jsonRes[j].display_name);
+                    let coords = JSON.stringify(jsonRes[j].geojson.coordinates);
+                    let type = JSON.stringify(jsonRes[j].geojson.type);
+                    let boundingBox = jsonRes[j].boundingbox;
+                    let q = `INSERT INTO rivers (riverId, name, coords, type, bbLatMin, bbLatMax, bbLngMin, bbLngMax) VALUES (${i}, ${sql(name)}, ${sql(coords)}, ${sql(type)}, ${boundingBox[0]},${sql(boundingBox[1])},${JSON.stringify(boundingBox[2])},${JSON.stringify(boundingBox[3])})`;
+                    let resql = await stmt.exec(q);
+                }
+            }
+
+            await stmt.commit();
+
         } catch (e) {
 
         }
     }
-
     await stmt.close();
 }
 
